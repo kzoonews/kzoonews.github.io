d4087493a1a93bb3663e0e36e1a5926ed49f3177
Morgan Kothman <abkothman@gmail.com>
2025-03-13 22:20:15 -0400

    First commit

---
+++ haunt.scm
@@ -0,0 +1,18 @@
+(use-modules (haunt asset)
+             (haunt builder blog)
+             (haunt builder atom)
+             (haunt builder assets)
+             (haunt reader commonmark)
+             (haunt site))
+
+(site #:title "Kzoo News"
+      #:domain "kzoo.news"
+      #:default-metadata
+      '((author . "Morgan Kothman")
+        (email  . "kzoo.news@proton.me"))
+      #:build-directory "docs"
+      #:readers (list commonmark-reader)
+      #:builders (list (blog)
+                       (atom-feed)
+                       (atom-feeds-by-tag)
+                       (static-directory "images")))
